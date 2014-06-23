3444833903aa190cfe75b66452daa9684d8039f6
Andrew Whitaker <aawhitaker@gmail.com>
2014-06-23 10:46:12 -0400

    Fixing an issue where adding a new work item would cause a JS error. Also showing work item ID in task card titles.

--- tfs_enhancer.js
+++ tfs_enhancer.js
@@ -18,26 +18,33 @@
 					Array.prototype.push.apply(nodesToProcess, n.querySelectorAll('div.tbTile'));
 				} else if (n.nodeType === 1 && n.matches('div.tbTile')) {
 					nodesToProcess.push(n);
-				}				
+				}
 			});
 			
 			processWorkItems(nodesToProcess);
 		});
 	}
 	
-	function modifyWorkItem(workItemId, workItemType) {
-		var el = qs('#tile-' + workItemId)
-			, title = el.querySelector('div.witTitle');
-			
-		title.innerText = (workItemType + ': ') + title.innerText;
+	function modifyWorkItem(id, type, title) {
+		var el = qs('#tile-' + id)
+			, titleEl = el.querySelector('div.witTitle');
+		
+		titleEl.innerText = (type + ' ' + id + ': ') + title;
 	}
 	
 	function getWorkItemData(workItemIds) {
 		var workItemStore = TFS.OM.TfsTeamProjectCollection.getDefaultConnection().getService(TFS.WorkItemTracking.WorkItemStore);
 
 		workItemStore.workItemManager.store.beginGetWorkItemData(workItemIds, function (result) {
+			if (result instanceof Array === false) {
+				result = [result];
+			}
 			result.forEach(function (workItemData) {
-				modifyWorkItem(workItemData.fields[-3], workItemData.fields[25]);
+				modifyWorkItem(
+					workItemData.fields[-3], 
+					workItemData.fields[25],
+					workItemData.fields[1]
+				);
 			});
 		});
 	}
@@ -51,15 +58,15 @@
 				var id = tile.id.replace('tile-', '');
 				
 				if (taskData.hasOwnProperty(id)) {
-					modifyWorkItem(id, taskData[id][1]);								
+					modifyWorkItem(id, taskData[id][1], taskData[id][3]);								
 				} else {
 					otherWorkItems.push(id);
 				}
-
-				if (otherWorkItems.length) {
-					getWorkItemData(otherWorkItems);
-				}
 			});
+			
+			if (otherWorkItems.length) {
+				getWorkItemData(otherWorkItems);
+			}			
 	}
 
 	if (typeof(taskboardEl) !== 'undefined') {
